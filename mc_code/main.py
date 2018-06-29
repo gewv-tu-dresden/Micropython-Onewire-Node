76627d787b6704c8ee025c6acee4f4d2631825d1
Falk Eusewig <falk.eusewig@tu-dresden.de>
2018-06-29 13:35:24 +0000

    main.py edited @ Falk Eusewig --> Lauffähig

--- mc_code/main.py
+++ mc_code/main.py
@@ -8,7 +8,7 @@ def printid(array):
         print(hex(array[i]))
 
 print('*******************Testprogramm DS2480B************************')
-VAR = DS2480b(DEBUG=1)
+VAR = DS2480b()
 VAR.initrs232(1)
 
 print("search...")
@@ -16,17 +16,13 @@ VAR.getallid()
 VAR.debug = 1
 clientno = VAR.checkdevices()
 VAR.debug = 0
-for i in range(0, 1000):
+j=0
+for i in range(0, 50):
     VAR.converttemp()
+    sleep(2)
     print("Abfrage " + str(i) + ": ")
     for j in range(0, clientno-1):
-        printid(VAR.romstorage[j])
-        print(VAR.acquiretemp(j))
+        print(VAR.romstorage[j], end="")
+        print(VAR.acquiretemp(j), end="")
         print("'C'")
-    #sleep(1)
-
 VAR.closers232()
-
-#print hex(VAR.Temperature)
-
-#print var.temperature
