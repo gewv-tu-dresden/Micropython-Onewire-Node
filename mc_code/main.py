5a2a0a36fdb7172268ae37cd97a44ef1eb0b3a75
Falk Eusewig <falk.eusewig@tu-dresden.de>
2018-07-02 13:31:44 +0000

    main.py edited @ Falk Eusewig --> sinnvolles Testprogramm mit Taster&LED

--- mc_code/main.py
+++ mc_code/main.py
@@ -1,5 +1,12 @@
 #from time import sleep
 from ds2480b import *
+from machine import Pin
+
+#'config IO'
+myled = Pin('P2', mode=Pin.OUT)
+myswitch = Pin('P8', mode=Pin.IN, pull=Pin.PULL_UP)
+
+myled.value(1)
 
 def printid(array):
     "print sensor id in hex"
@@ -17,12 +24,30 @@ VAR.debug = 1
 clientno = VAR.checkdevices()
 VAR.debug = 0
 j=0
-for i in range(0, 50):
-    VAR.converttemp()
-    sleep(2)
-    print("Abfrage " + str(i) + ": ")
-    for j in range(0, clientno-1):
-        print(VAR.romstorage[j], end="")
-        print(VAR.acquiretemp(j), end="")
-        print("'C'")
+i=0
+go = 1
+riegel = 1
+while (1):
+    myled.toggle()
+    sleep(0.2)
+    if myswitch()==0 and riegel ==1:
+        print ('Taster')
+        riegel = 0
+        if go == 1:
+            print('Abfrage AUS')
+            go = 0
+        else:
+            print('Abfrage EIN')
+            go = 1
+    if myswitch()==1 and riegel ==0:
+        riegel = 1
+    if go == 1:
+        i+=1
+        VAR.converttemp()
+        print("Abfrage " + str(i) + ": ")
+        sleep(0.8)
+        for j in range(0, clientno-1):
+            print(VAR.romstorage[j], end="")
+            print(VAR.acquiretemp(j), end="")
+            print("'C'")
 VAR.closers232()
