c5487b85f8c6e7de215ddfa5c232d05883fb366b
Karl Wolffgang <karl_eugen.wolffgang@tu-dresden.de>
2018-07-16 14:57:57 +0200

    Add lora code to connect to the the thingsnetwork.

--- mc_code/boot.py
+++ mc_code/boot.py
@@ -13,7 +13,7 @@ uart = machine.UART(0, 115200) # disable these two lines if you don't want seria
 os.dupterm(uart)
 
 # LoRa in LORAWAN mode.
-#lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
+lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
 
 # Initialize watchdog
 #wdt = WDT(timeout=60000)
@@ -25,33 +25,32 @@ pycom.rgbled(0x220000) # red
 
 ### Configure Lora ###
 if machine.reset_cause() == machine.WDT_RESET:
-    debug_log("Watchdog rescued us.")
+    print("Watchdog rescued us.")
 
 if machine.reset_cause() == machine.DEEPSLEEP_RESET:
-    debug_log("Woke up from deepsleep.")
+    print("Woke up from deepsleep.")
     lora.nvram_restore()
 
 if machine.reset_cause() == machine.DEEPSLEEP_RESET and lora.has_joined():
-    debug_log("Skipping joining.")
+    print("Skipping joining.")
 else:
-    pass
-    #debug_log("Try to join TTN")
+    print("Try to join TTN")
     # create an OTAA authentication parameters
-    #dev_eui = binascii.unhexlify('005CEF018F9012A7')
-    #app_eui = binascii.unhexlify('70B3D57ED0006FD4')
-    #app_key = binascii.unhexlify('CBD1B7A0C667F2CE64B16098BD1A5AFE')
+    dev_eui = binascii.unhexlify('004BA9B9A7199F28')
+    app_eui = binascii.unhexlify('70B3D57ED00109EF')
+    app_key = binascii.unhexlify('B0B863F19717BB0BB5D73370BA08A252')
 
     # join a network using OTAA (Over the Air Activation)
-    #lora.join(activation=LoRa.OTAA, auth=(dev_eui, app_eui, app_key), timeout=0)
+    lora.join(activation=LoRa.OTAA, auth=(dev_eui, app_eui, app_key), timeout=0)
 
     # wait until the module has joined the network
-    #while not lora.has_joined():
-    #    time.sleep(2.5)
-    #    debug_log('Not yet joined...')
+    while not lora.has_joined():
+        time.sleep(2.5)
+        print('Not yet joined...')
 
-    #debug_log('Joined.')
+    print('Joined.')
 
 # create a LoRa socket
-#s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
-#s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
 ######################
--- mc_code/main.py
+++ mc_code/main.py
@@ -1,12 +1,16 @@
 #from time import sleep
 from ds2480b import *
 from machine import Pin
+from cayennelpp import CayenneLPP
 
 #'config IO'
 myled = Pin('P2', mode=Pin.OUT)
 switch1 = Pin('P8', mode=Pin.IN, pull=Pin.PULL_UP)
 switch2 = Pin('P23', mode=Pin.IN, pull=Pin.PULL_UP)
 myled.value(1)
+
+# Cayenne LowPowerPayload
+c = CayenneLPP()
 
 def printid(array):
     "print sensor id in hex"
@@ -23,15 +27,20 @@ VAR.getallid()
 VAR.debug = 1
 clientno = VAR.checkdevices()
 VAR.debug = 0
-j=0
-i=0
+
 go = 1
 riegel = 1
+turns = 0
+
 while (1):
     myled.toggle()
     sleep(0.2)
+
+    # only every 30 reads a package should send
+    sendThisTurn = i % 30 == 0
+
     if switch1()==0 and riegel ==1:
-        print ('switch1')
+        print('switch1')
         riegel = 0
         if go == 1:
             print('Abfrage AUS')
@@ -39,18 +48,33 @@ while (1):
         else:
             print('Abfrage EIN')
             go = 1
+
     if switch1()==1 and riegel ==0:
         riegel = 1
+
     if go == 1:
         i+=1
         VAR.converttemp()
         print("Abfrage " + str(i) + ": ")
         sleep(0.8)
         for j in range(0, clientno-1):
-            print(VAR.romstorage[j], end="")
-            print(VAR.acquiretemp(j), end="")
-            print("'C'")
+            rom_storage = VAR.romstorage[j]
+            acq_temp = VAR.acquiretemp(j)
+
+            # only every 30 reads a package should send
+            if sendThisTurn:
+                c.addTemperature(j, acq_temp)
+
+            print("{} {} 'C'".format(rom_storage, acq_temp))
+
     if switch2()==0:
-        print ("switch2")
+        print("switch2")
         break
+
+    if sendThisTurn:
+        print("Send temps to app server.")
+        s.setblocking(True)
+        s.send(c.getBuffer())
+        s.setblocking(False)
+
 VAR.closers232()
